80a3862cb91c751d63e0037dc752af4d36fe5b41
mac <mac@ugjikeom.local>
2018-04-01 15:06:28 +0900

    Add video second, slider View

--- gestureVideo/ViewController.swift
+++ gestureVideo/ViewController.swift
@@ -22,8 +22,10 @@ class ViewController: UIViewController{
         super.viewDidLoad()
         
         let url = URL(string: "https://s3.ap-northeast-2.amazonaws.com/project-sm/%EC%B2%AD%ED%95%98+(CHUNG+HA)+-+Roller+Coaster+MV.mp4")!
+        
         player = AVPlayer(url: url)
         player.currentItem?.addObserver(self, forKeyPath: "duration", options: [.new,.initial], context: nil)
+        addTimeObserver()
         playerLayer = AVPlayerLayer(player: player)
         playerLayer.videoGravity = .resize
         
@@ -66,6 +68,35 @@ class ViewController: UIViewController{
         
     }
     @IBAction func sliderChangeAction(_ sender: UISlider) {
+        player.seek(to: CMTimeMake(Int64(sender.value*1000), 1000))
+    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "duration", let duration = player.currentItem?.duration.seconds, duration > 0.0 {
+            self.durationLbl.text = getTimeString(from: player.currentItem!.duration)
+        }
+    }
+    func getTimeString(from time:CMTime) -> String {
+        let totalSeconds = CMTimeGetSeconds(time)
+        let hours = Int(totalSeconds/3600)
+        let minutes = Int(totalSeconds/60) % 60
+        let seconds = Int(totalSeconds.truncatingRemainder(dividingBy: 60))
+        if hours < 0{
+            return String(format: "%i:%02i:%02i", arguments: [hours,minutes,seconds])
+        }else{
+            return String(format: "%02i:%02i", arguments: [minutes,seconds])
+        }
+    }
+    func addTimeObserver(){
+        let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        let mainQueue = DispatchQueue.main
+        _ = player.addPeriodicTimeObserver(forInterval: interval, queue: mainQueue, using: {[weak self]
+            time in
+            guard let currentItem = self?.player.currentItem else {return}
+            self?.timeSlider.maximumValue = Float(currentItem.duration.seconds)
+            self?.timeSlider.minimumValue = 0
+            self?.timeSlider.value = Float(currentItem.currentTime().seconds)
+            self?.currentLbl.text = self?.getTimeString(from: currentItem.currentTime())
+        })
     }
 }
 
